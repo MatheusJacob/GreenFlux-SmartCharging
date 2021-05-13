143431bcfbfe5088bac4e2a475489697afb09537
Matheus Jacob <matheus.jacob_external@capgemini.com>
2021-05-13 07:27:39 +0100

    Restructure solution folder and adding specflow unit + integration template tests

---
+++ tests/GreenFlux.SmartCharging.Matheus.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GreenFlux.SmartCharging.Matheus.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
