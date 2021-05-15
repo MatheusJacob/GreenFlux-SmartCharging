703a91ed4fc469293433ce00d68f25ee5c1affd2
Matheus Jacob <matheus.jacob_external@capgemini.com>
2021-05-15 12:03:45 +0100

    Added Validatorfilter + Save group validator

---
+++ src/GreenFlux.SmartCharging.Matheus.API/Validators/SaveGroupValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using GreenFlux.SmartCharging.Matheus.API.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreenFlux.SmartCharging.Matheus.API.Validators
+{
+    public class SaveGroupValidator : AbstractValidator<SaveGroupResource>
+    {
+        public SaveGroupValidator()
+        {
+            RuleFor(g => g.Name).NotEmpty().WithMessage("'Name' field is required");
+            RuleFor(g => g.Capacity).NotNull().WithMessage("'Capacity' field is required");
+            RuleFor(g => g.Capacity).GreaterThan(0).WithMessage("'Capacity' field must be greater than 0");
+            RuleFor(g => g.Capacity).LessThan(float.MaxValue).WithMessage($"'Capacity' field must be less than {float.MaxValue.ToString()}");
+        }
+    }
+}
