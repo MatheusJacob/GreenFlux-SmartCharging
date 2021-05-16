57bc705fe8e0eba14b7c4d87f034f2bb10e01f76
MatheusJacob <matheus_paulin11@hotmail.com>
2021-05-16 15:36:49 +0100

    Fragmented route to be able to reuse on tests

--- src/GreenFlux.SmartCharging.Matheus.API/Controllers/Routes.cs
+++ src/GreenFlux.SmartCharging.Matheus.API/Controllers/Routes.cs
@@ -8,9 +8,12 @@ namespace GreenFlux.SmartCharging.Matheus.API.Controllers
     public static class Routes
     {
         public const string ApiBaseRoute = "api";
+        public const string ChargeStationBaseRoute = "/chargeStations";
+        public const string GroupBaseRoute = "/groups";
+        public const string ConnectorsBaseRoute = "/connectors";
 
-        public const string GroupsRoute = ApiBaseRoute + "/groups";
-        public const string ChargeStationsRoute = ApiBaseRoute + "/groups/{groupId}/chargeStations";
-        public const string ConnectorsRoute = ApiBaseRoute + "/groups/{groupId}/chargeStations/{chargeStationId}/connectors";
+        public const string GroupsRoute = ApiBaseRoute + GroupBaseRoute;
+        public const string ChargeStationsRoute = ApiBaseRoute + GroupBaseRoute  + "/{groupId}" + ChargeStationBaseRoute;
+        public const string ConnectorsRoute = ApiBaseRoute + GroupBaseRoute + "/{groupId}" + ChargeStationBaseRoute + "{chargeStationId}" + ConnectorsBaseRoute;
     }
 }
