4125251f765eb66e62bc9672629de90c77d7749f
MatheusJacob <matheus_paulin11@hotmail.com>
2021-05-19 11:00:26 +0100

    Added exceptions capacity exceeded and no slots available

---
+++ src/GreenFlux.SmartCharging.Matheus.Domain/Exceptions/CapacityExceededException.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenFlux.SmartCharging.Matheus.Domain.Models
+{
+    public class CapacityExceededException : ProblemDetails
+    {
+        public readonly float ExceededCapacity;
+        public CapacityExceededException(float exceededCapacity)
+        {
+            Title = "Capacity exceeded";
+            Status = 400;
+            Type = typeof(CapacityExceededException).ToString();
+            Detail = $"The group capacity has exceeded by {exceededCapacity}A, remove the suggested connectors to free up space";
+            ExceededCapacity = exceededCapacity;
+        }
+    }
+}
---
+++ src/GreenFlux.SmartCharging.Matheus.Domain/Exceptions/NoSlotsAvailableException.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenFlux.SmartCharging.Matheus.Domain.Exceptions
+{
+    public class NoSlotsAvailableException : ProblemDetails
+    {
+        private Guid ChargeStationId { get; set; }
+        public NoSlotsAvailableException(Guid chargeStationId)
+        {
+            Title = "No slots available for the Charge Station";
+            Status = 400;
+            Type = typeof(NoSlotsAvailableException).ToString();
+            Detail = $"No slots available for the Charge Station : {chargeStationId}";
+            ChargeStationId = chargeStationId;
+        }
+    }
+}
